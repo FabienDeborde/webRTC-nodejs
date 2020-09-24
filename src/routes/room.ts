d827e90c43d26570d24c1977221905b28d078b6f
Fabien <fabien.webdev@gmail.com>
2020-09-24 21:45:26 +0900

    fix room id encoding in responses & add name in responses

--- src/routes/room.ts
+++ src/routes/room.ts
@@ -9,9 +9,11 @@ const router = express.Router()
 router.get('/', async (req, res) => {
   const rooms = await Room.findAll()
   const encrypted = encrypt('5f6a9b568e81634c48f10319')
+  const encoded = encodeURIComponent(encrypt('5f6a9b568e81634c48f10319'))
   const decrypted = decrypt(encrypted)
   return res.send({
     encrypted,
+    encoded,
     decrypted,
     rooms
   })
@@ -30,16 +32,17 @@ router.post('/access', async (req, res) => {
   if (!id) return res.badRequest('No room specified.')
   try {
     const room = await Room.findOne(decrypt(decodeURIComponent(id))) as RoomDocument
+    const roomID: string = encodeURIComponent(encrypt(room && room.id))
     if (room.password) {
       if (!password) return res.unauthorized('You do not have access to this room. Please contact your administrator for further information.')
       if (compareHash(password, room.password)) {
-        return res.send({ access: true, id: room.id })
+        return res.send({ access: true, id: roomID, name: room.name })
       } else {
         await BlockUser.createOrUpdateOne(req.clientIP)
         return res.forbidden('You do not have access to this room. Please contact your administrator for further information.')
       }
     } else {
-      return res.send({ access: true, id: room.id })
+      return res.send({ access: true, id: roomID, name: room.name })
     }
   } catch (error) {
     return res.internalServerError('An error occured while retrieving the room', error)
@@ -55,7 +58,8 @@ router.post('/', async (req, res) => {
     const id: string = encodeURIComponent(encrypt(response && response.id))
     return res.send({
       // decrypted: decrypt(decodeURIComponent(id)),
-      id
+      id,
+      name
     })
   } catch (error) {
     return res.internalServerError('An error occured while creating the room', error)
